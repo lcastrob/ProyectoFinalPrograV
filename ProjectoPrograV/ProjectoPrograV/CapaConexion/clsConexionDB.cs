02c987175aabfd906ea6c8d54e169624cfbdb3d5
loganlh <johaolh@hotmail.com>
2015-03-23 20:09:20 -0600

    creacion de clase de conexion y front end and back end tipo usuario

---
+++ ProjectoPrograV/ProjectoPrograV/CapaConexion/clsConexionDB.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectoPrograV.CapaConexion
+{
+    class clsConexionDB
+    {
+  String stringConexion;
+        String usuario;
+        String baseDatos;
+        String servidor;
+        SqlConnection miConexion = new SqlConnection();
+        SqlCommand miSqlCommand = new SqlCommand();
+
+        private void abrirConexion()
+        {
+            miConexion.ConnectionString = " server= . ; initial catalog = ManejaDocumentos1; integrated security  = true";
+            miConexion.Open();
+        
+        }
+
+        public void ejecutarSentenciaSql(String sqlCommandTex, SqlParameter [] misParametros)
+       {
+           abrirConexion();
+           miSqlCommand.CommandType = System.Data.CommandType.Text;
+           miSqlCommand.Connection = this.miConexion;
+           miSqlCommand.CommandText = sqlCommandTex;
+
+           miSqlCommand.Parameters.AddRange( misParametros);
+           miSqlCommand.ExecuteNonQuery();
+           this.miConexion.Close();
+
+        }
+
+        public SqlDataReader consultarInformacion(String sqlCommandTex, SqlParameter[] misParametros)
+        {
+            miSqlCommand = new SqlCommand();
+            abrirConexion();
+            miSqlCommand.CommandType = System.Data.CommandType.Text;
+            miSqlCommand.Connection = this.miConexion;
+            miSqlCommand.CommandText = sqlCommandTex;
+
+            if (misParametros !=  null)
+                miSqlCommand.Parameters.AddRange(misParametros);
+            SqlDataReader miDr =  miSqlCommand.ExecuteReader();
+            return miDr;
+            
+        }
+    }
+}
